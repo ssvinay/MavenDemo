3f102580ef63980b7bd8ca29060aefa56a2f0eec
Vinay <Vinay@Dell>
2018-04-15 13:41:08 +0530

    Test

---
+++ src/test/java/Sample/TestPrint.java
@@ -0,0 +1,15 @@
+package Sample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestPrint 
+{
+
+	@Test
+	public void m1()
+	{
+		Reporter.log("How are you",true);
+	}
+	
+}
